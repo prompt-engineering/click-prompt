def6c8960a11b05191b672dd650b2b3230fe9419
Phodal Huang <h@phodal.com>
2023-03-03 21:54:41 +0800

    docs: add time for sd

--- src/pages/StableDiffusionGenerator/index.tsx
+++ src/pages/StableDiffusionGenerator/index.tsx
@@ -1,15 +1,15 @@
-import React from "react";
+import React from 'react'
 import {
   Button,
   FormControl,
   FormLabel,
   Select,
   SimpleGrid,
-  Text,
-} from "@chakra-ui/react";
-import { Form, Formik } from "formik";
-import sdImage from "@/assets/stable-diffusion-demo.jpeg";
-import Image from "next/image";
+  Text
+} from '@chakra-ui/react'
+import { Form, Formik } from 'formik'
+import sdImage from '@/assets/stable-diffusion-demo.jpeg'
+import Image from 'next/image'
 
 type SdPromptField = {
   name: string;
@@ -22,102 +22,107 @@ type SdPromptField = {
 /* FormControl: 头发, 眼睛, 脸型, 身体, 胸部, 腰部, 腿部, 脚部 */
 const sdDetailedPromptFields: SdPromptField[] = [
   {
-    name: "hair",
-    label: "头发",
-    selectValues: ["Long", "Short", "Bald"],
+    name: 'hair',
+    label: '头发',
+    selectValues: ['Long', 'Short', 'Bald']
   },
   {
-    name: "eyes",
-    label: "眼睛",
-    selectValues: ["Normal", "Black", "Red"],
+    name: 'eyes',
+    label: '眼睛',
+    selectValues: ['Normal', 'Black', 'Red']
   },
   {
-    name: "face",
-    label: "脸型",
-    selectValues: ["Normal", "Round", "Square"],
+    name: 'face',
+    label: '脸型',
+    selectValues: ['Normal', 'Round', 'Square']
   },
   {
-    name: "body",
-    label: "身体",
-    selectValues: ["Normal", "Fat", "Thin"],
+    name: 'body',
+    label: '身体',
+    selectValues: ['Normal', 'Fat', 'Thin']
   },
   {
-    name: "chest",
-    label: "胸部",
-    selectValues: ["Normal", "Large", "Small"],
+    name: 'chest',
+    label: '胸部',
+    selectValues: ['Normal', 'Large', 'Small']
   },
   {
-    name: "waist",
-    label: "腰部",
-    selectValues: ["Normal", "Large", "Small"],
+    name: 'waist',
+    label: '腰部',
+    selectValues: ['Normal', 'Large', 'Small']
   },
   {
-    name: "legs",
-    label: "腿部",
-    selectValues: ["Normal", "Large", "Small"],
+    name: 'legs',
+    label: '腿部',
+    selectValues: ['Normal', 'Large', 'Small']
   },
   {
-    name: "feet",
-    label: "脚部",
-    selectValues: ["Normal", "Large", "Small"],
-  },
-];
+    name: 'feet',
+    label: '脚部',
+    selectValues: ['Normal', 'Large', 'Small']
+  }
+]
 
 /* FormControl: 通用质量,光线,镜头,头部,场景,身体,服饰,动作,背景 */
 const sdPromptFields: SdPromptField[] = [
   {
-    name: "quality",
-    label: "通用质量",
-    selectValues: ["Normal", "High"],
+    name: 'quality',
+    label: '通用质量',
+    selectValues: ['Normal', 'High']
   },
   {
-    name: "light",
-    label: "光线",
-    selectValues: ["Daylight", "Night", "Indoor"],
+    name: 'time',
+    label: '时间',
+    selectValues: ['Morning', 'Afternoon', 'Evening', 'Night']
   },
   {
-    name: "camera",
-    label: "镜头",
-    selectValues: ["Front", "Back", "Side"],
+    name: 'light',
+    label: '光线',
+    selectValues: ['Daylight', 'Night', 'Indoor']
   },
   {
-    name: "head",
-    label: "头部",
-    selectValues: ["Front", "Back", "Side"],
+    name: 'camera',
+    label: '镜头',
+    selectValues: ['Front', 'Back', 'Side']
   },
   {
-    name: "scene",
-    label: "场景",
-    selectValues: ["Front", "Back", "Side"],
+    name: 'head',
+    label: '头部',
+    selectValues: ['Front', 'Back', 'Side']
   },
   {
-    name: "body",
-    label: "身体",
-    selectValues: ["Front", "Back", "Side"],
+    name: 'scene',
+    label: '场景',
+    selectValues: ['Front', 'Back', 'Side']
   },
   {
-    name: "clothes",
-    label: "服饰",
-    selectValues: ["Chinese Style", "Back", "Side"],
+    name: 'body',
+    label: '身体',
+    selectValues: ['Front', 'Back', 'Side']
   },
   {
-    name: "action",
-    label: "动作",
-    selectValues: ["Front", "Back", "Side"],
+    name: 'clothes',
+    label: '服饰',
+    selectValues: ['Chinese Style', 'Back', 'Side']
   },
   {
-    name: "background",
-    label: "背景",
-    selectValues: ["Front", "Back", "Side"],
+    name: 'action',
+    label: '动作',
+    selectValues: ['Front', 'Back', 'Side']
   },
-];
+  {
+    name: 'background',
+    label: '背景',
+    selectValues: ['Front', 'Back', 'Side']
+  }
+]
 
 function StableDiffusionGenerator() {
-  const [isSubmitting, setIsSubmitting] = React.useState(false);
+  const [isSubmitting, setIsSubmitting] = React.useState(false)
 
   return (
     <div>
-      <Image src={sdImage} alt='stable-diffusion-demo' />
+      <Image src={ sdImage } alt='stable-diffusion-demo' />
       <Text>
         Pormpt： particle effects small breasts, 1 girl, solo, masterpiece, best
         quality, highres, original, extremely detailed 8K wallpaper, greasy
@@ -130,46 +135,46 @@ function StableDiffusionGenerator() {
         detailed eyes, peach blossom
       </Text>
       <Formik
-        initialValues={{ quality: "Normal" }}
-        onSubmit={(values, actions) => {
+        initialValues={ { quality: 'Normal' } }
+        onSubmit={ (values, actions) => {
           setTimeout(() => {
-            alert(JSON.stringify(values, null, 2));
-            actions.setSubmitting(false);
-          }, 1000);
-        }}
+            alert(JSON.stringify(values, null, 2))
+            actions.setSubmitting(false)
+          }, 1000)
+        } }
       >
         <Form>
-          <SimpleGrid gap={12} p={12} columns={3}>
-            {sdPromptFields.map((field) => (
-              <FormControl key={field.name} id={field.name} mt={4}>
-                <FormLabel>{field.label}</FormLabel>
-                <Select name={field.name} placeholder={`Select ${field.label}`}>
-                  {field.selectValues.map((value) => (
-                    <option key={value} value={value}>
-                      {value}
+          <SimpleGrid gap={ 12 } p={ 12 } columns={ 3 }>
+            { sdPromptFields.map((field) => (
+              <FormControl key={ field.name } id={ field.name } mt={ 4 }>
+                <FormLabel>{ field.label }</FormLabel>
+                <Select name={ field.name } placeholder={ `Select ${ field.label }` }>
+                  { field.selectValues.map((value) => (
+                    <option key={ value } value={ value }>
+                      { value }
                     </option>
-                  ))}
+                  )) }
                 </Select>
               </FormControl>
-            ))}
+            )) }
             <Text>Body</Text>
-            {sdDetailedPromptFields.map((field) => (
-              <FormControl key={field.name} id={field.name} mt={4}>
-                <FormLabel>{field.label}</FormLabel>
-                <Select name={field.name} placeholder={`Select ${field.label}`}>
-                  {field.selectValues.map((value) => (
-                    <option key={value} value={value}>
-                      {value}
+            { sdDetailedPromptFields.map((field) => (
+              <FormControl key={ field.name } id={ field.name } mt={ 4 }>
+                <FormLabel>{ field.label }</FormLabel>
+                <Select name={ field.name } placeholder={ `Select ${ field.label }` }>
+                  { field.selectValues.map((value) => (
+                    <option key={ value } value={ value }>
+                      { value }
                     </option>
-                  ))}
+                  )) }
                 </Select>
               </FormControl>
-            ))}
+            )) }
           </SimpleGrid>
           <Button
-            mt={4}
+            mt={ 4 }
             colorScheme='teal'
-            isLoading={isSubmitting}
+            isLoading={ isSubmitting }
             type='submit'
           >
             Generator
@@ -177,7 +182,7 @@ function StableDiffusionGenerator() {
         </Form>
       </Formik>
     </div>
-  );
+  )
 }
 
-export default StableDiffusionGenerator;
+export default StableDiffusionGenerator
