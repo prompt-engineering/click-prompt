63bc529240774cb188a656560a0c64bc0462576f
Phodal Huang <h@phodal.com>
2023-03-03 15:26:25 +0800

    chore: use react router for routes

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,19 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 import { ChakraProvider } from '@chakra-ui/react'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <ChakraProvider resetCSS={true}>
-      <App />
+      <RouterProvider router={router} />
     </ChakraProvider>
   </React.StrictMode>
 )
