651f4863c3cdd487be67bf568e1a4e4662c17cb6
Phodal Huang <h@phodal.com>
2023-03-04 20:07:31 +0800

    feat: add generate scripts for chatgpt samples

---
+++ gen/generate-samples-index.js
@@ -0,0 +1,27 @@
+// 1. walkdir of src/assets/chatgpt/templates to get all yaml files
+// 2. for each yaml file, read the file and parse the yaml
+// 3. save the {name, description, path, author} to a json file: index.json
+const fs = require("fs");
+const path = require("path");
+const walkdir = require("walkdir");
+const yaml = require("js-yaml");
+
+const templatesDir = path.join(__dirname, "../src/assets/chatgpt/templates");
+const indexFile = path.join(templatesDir, "index.json");
+
+const files = walkdir.sync(templatesDir, { no_recurse: true });
+const index = files
+  .filter((f) => f.endsWith(".yaml") || f.endsWith(".yml"))
+  .map((f) => {
+    const content = fs.readFileSync(f, "utf8");
+    const doc = yaml.load(content);
+    const { name, description, author } = doc;
+    return {
+      name,
+      description,
+      author,
+      path: path.relative(templatesDir, f),
+    };
+  });
+
+fs.writeFileSync(indexFile, JSON.stringify(index, null, 2));
