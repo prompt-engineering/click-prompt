f800dabfde5d528c1fef9b474b0bec86ddb322d7
Phodal Huang <h@phodal.com>
2023-03-04 14:54:30 +0800

    feat: make color pick works

--- src/components/SimpleColorPicker.tsx
+++ src/components/SimpleColorPicker.tsx
@@ -1,9 +1,31 @@
 import React from 'react'
-import { CompactPicker, SketchPicker } from 'react-color'
+import { ChromePicker, ColorResult, SwatchesPicker } from 'react-color'
 import styled from '@emotion/styled'
+import nearestColor from 'nearest-color'
+import colorNameList from 'color-name-list'
 
-function SimpleColorPicker() {
-  const [color, setColor] = React.useState('rgba(241, 112, 19, 1)')
+export enum ColorType {
+  HumanSkin = 'HumanSkin',
+  // Normal Safe Colors
+  Normal = 'Normal',
+}
+
+export enum HumanSkinColor {
+  White = 'rgba(255, 255, 255, 1)',
+  Black = 'rgba(0, 0, 0, 1)',
+  Yellow = 'rgba(255, 255, 0, 1)',
+  Brown = 'rgba(255, 165, 0, 1)',
+  DarkBrown = 'rgba(139, 69, 19, 1)',
+}
+
+type SimpleColorProps = {
+  colorType?: ColorType;
+
+  updateColor?: (color: string) => void;
+}
+
+function SimpleColorPicker(props: SimpleColorProps) {
+  const [color, setColor] = React.useState('rgba(255, 255, 255, 1))')
   const [displayColorPicker, setDisplayColorPicker] = React.useState(false)
 
   const handleClick = () => {
@@ -14,8 +36,16 @@ function SimpleColorPicker() {
     setDisplayColorPicker(false)
   }
 
-  const handleChange = (color: any) => {
-    setColor(`rgba(${ color.rgb.r }, ${ color.rgb.g }, ${ color.rgb.b }, ${ color.rgb.a })`)
+  const colors = colorNameList.reduce((o, { name, hex }) => Object.assign(o, { [name]: hex }), {});
+  const nearest = nearestColor.from(colors);
+  const handleChange = (color: ColorResult) => {
+    let newColor = `rgba(${ color.rgb.r }, ${ color.rgb.g }, ${ color.rgb.b }, ${ color.rgb.a })`
+    setColor(newColor)
+    if (props.updateColor) {
+      let colorName = nearest(color.hex).name
+      console.log("nearest color: " + colorName)
+      props.updateColor(colorName)
+    }
   }
 
   return (
@@ -24,7 +54,7 @@ function SimpleColorPicker() {
         <StyleColor color={ color } /></Swatch>
       { displayColorPicker ? <StylePopover>
         <StyleCover onClick={ handleClose } />
-        <CompactPicker color={ color } onChange={ handleChange } />
+        <ChromePicker color={ color } onChange={ handleChange } />
       </StylePopover> : null }
     </>
   )
@@ -34,7 +64,9 @@ const StyleColor = styled.div`
   width: 16px;
   height: 14px;
   border-radius: 2px;
-  background: ${ props => props.color };
+  background: ${
+    props => props.color
+  };
 `
 
 const Swatch = styled.div`
--- src/pages/StableDiffusionGenerator/PromptFieldForm.tsx
+++ src/pages/StableDiffusionGenerator/PromptFieldForm.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import { FormControl, FormLabel, Select } from '@chakra-ui/react'
-import SimpleColorPicker from '@/components/SimpleColorPicker'
+import SimpleColorPicker, { ColorType } from '@/components/SimpleColorPicker'
 import { FormikProps } from 'formik'
 
 type SelectValue = {
@@ -26,16 +26,27 @@ type FieldFormProp = {
 
 export function PromptFieldForm(props: FieldFormProp) {
   const { field, formik } = props
-  const [colorName, setColorName] = useState('')
+  const [color, setColor] = useState('')
 
   return <FormControl key={ field.name } id={ field.name } mt={ 2 }>
     <FormLabel>
-      { field.label } { field.colored && <SimpleColorPicker /> }
+      { field.label } { field.colored &&
+      <SimpleColorPicker colorType={ ColorType.Normal } updateColor={ (color) => setColor(color) } />
+    }
     </FormLabel>
 
     <Select name={ field.name }
             placeholder={ `Select ${ field.label }` }
-            onChange={ formik.handleChange }>
+            onChange={ (event) => {
+              console.log(color)
+              if (color !== '') {
+                formik.setFieldValue(field.name, `${ color } ${ event.target.value }`)
+              } else {
+                formik.setFieldValue(field.name, event.target.value)
+              }
+            }
+            }>
+
       { field.selectValues.map((select, index) => (
         <option key={ field.name + '-' + index } value={ select.value }>
           { select.key }
