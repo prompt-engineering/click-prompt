29825c797c0bd2433a97f74c1623e420cbe3699c
Phodal Huang <h@phodal.com>
2023-03-05 09:06:53 +0800

    refactor: make enable display model

--- src/components/CopyComponent.tsx
+++ src/components/CopyComponent.tsx
@@ -2,7 +2,13 @@ import { CopyToClipboard } from "react-copy-to-clipboard";
 import { CopyIcon } from "@chakra-ui/icons";
 import React from "react";
 
-function CopyComponent({ value, className = "" }: { value: string; className?: string }) {
+type CopyProps = {
+  value: string;
+  className?: string;
+  children?: React.ReactNode;
+};
+
+function CopyComponent({ value, className = "", children }: CopyProps) {
   return (
     <div className={className}>
       <CopyToClipboard
@@ -12,6 +18,7 @@ function CopyComponent({ value, className = "" }: { value: string; className?: s
         }}
       >
         <div className='cursor-pointer flex justify-center'>
+          {children ? children : ""}
           <CopyIcon boxSize={22} />
         </div>
       </CopyToClipboard>
--- src/pages/stable-diffusion-examples/index.tsx
+++ src/pages/stable-diffusion-examples/index.tsx
@@ -1,38 +1,64 @@
 import React from "react";
 
-import { CardBody, Heading, SimpleGrid, Stack, Link } from "@chakra-ui/react";
-import { Card, CardHeader } from "@chakra-ui/card";
+import { Button, ButtonGroup, CardBody, Heading, Link, SimpleGrid, Stack, Text } from "@chakra-ui/react";
+import { Card, CardFooter, CardHeader } from "@chakra-ui/card";
 import Image from "next/image";
 
 import CopyComponent from "@/components/CopyComponent";
 import samples from "@/assets/stable-diffusion/samples/index.json";
 import { ExternalLinkIcon } from "@chakra-ui/icons";
+import { parseStableDiffusionPrompt } from "@/data-processor/SduiParser";
+
+type StableDiffusionSample = {
+  name: string;
+  author: string;
+  category: string;
+  homepage: string;
+  artists: {
+    preview: string;
+    prompt: string;
+  }[];
+  path: string;
+};
 
 function Index() {
+  function ArticleCard(index: number, sample: StableDiffusionSample, artist: { preview: string; prompt: string }) {
+    const parsedPrompt = parseStableDiffusionPrompt(artist.prompt);
+
+    return (
+      <Card key={`sample-${index}`}>
+        <CardHeader>
+          <Heading size='md'>
+            {sample.name} -{" "}
+            <Link href={sample.homepage} isExternal>
+              {sample.author} <ExternalLinkIcon />
+            </Link>
+          </Heading>
+        </CardHeader>
+        <CardBody>
+          <Image src={artist.preview} alt='' width={512} height={512} />
+          <Stack>
+            <Text>Model: {parsedPrompt.model}</Text>
+          </Stack>
+        </CardBody>
+        <CardFooter>
+          <ButtonGroup spacing='2'>
+            <Button variant='solid' colorScheme='blue'>
+              <CopyComponent value={artist.prompt}>复制</CopyComponent>
+            </Button>
+          </ButtonGroup>
+        </CardFooter>
+      </Card>
+    );
+  }
+
   return (
     <>
       {samples.length > 0 && (
         <SimpleGrid spacing={4} templateColumns='repeat(auto-fill, minmax(320px, 1fr))'>
           {samples.map((sample, index) => (
             <div key={"index-" + index}>
-              {sample.artists.map((artist, index) => (
-                <Card key={`sample-${index}`}>
-                  <CardHeader>
-                    <Heading size='md'>
-                      {sample.name} -{" "}
-                      <Link href={sample.homepage} isExternal>
-                        {sample.author} <ExternalLinkIcon />
-                      </Link>
-                    </Heading>
-                  </CardHeader>
-                  <CardBody>
-                    <Stack>
-                      <Image src={artist.preview} alt='' width={512} height={512} />
-                      <CopyComponent value={artist.prompt} />
-                    </Stack>
-                  </CardBody>
-                </Card>
-              ))}
+              {sample.artists.map((artist, index) => ArticleCard(index, sample, artist))}
             </div>
           ))}
         </SimpleGrid>
