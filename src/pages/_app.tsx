cc2a4c2850af02433e85d723aca87e5c101fd0ee
Jason <m.jason.liu@outlook.com>
2023-03-05 15:07:44 +0800

    chore: global footer vercel

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -23,6 +23,16 @@ export default function App({ Component, pageProps }: any) {
           </Container>
         </div>
       </ChakraProvider>
+      <div className='flex justify-center py-4'>
+        {/* Vercel footer */}
+        <a className='flex gap-4 items-center' href='https://vercel.com?utm_source=prompt-engineering&utm_campaign=oss'>
+          <span>Powered by</span>
+          <img
+            src='https://images.ctfassets.net/e5382hct74si/78Olo8EZRdUlcDUFQvnzG7/fa4cdb6dc04c40fceac194134788a0e2/1618983297-powered-by-vercel.svg'
+            alt='Vercel Logo'
+          />
+        </a>
+      </div>
     </>
   );
 }
