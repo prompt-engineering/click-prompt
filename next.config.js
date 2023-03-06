e1a3f0253295ef0157d7e75ba3e0fdec696bdaed
CGQAQ <m.jason.liu@outlook.com>
2023-03-06 17:27:28 +0800

    fix: fix svgr issue

--- next.config.js
+++ next.config.js
@@ -11,9 +11,17 @@ const nextConfig = {
       use: "yaml-loader",
     });
 
+    config.module.rules.push(    {
+      test: /\.svg$/i,
+      type: 'asset',
+      resourceQuery: /url/, // *.svg?url
+    });
+
     config.module.rules.push({
-      test: /\.c.svg$/,
-      use: ["@svgr/webpack"],
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      resourceQuery: { not: [/url/] }, // exclude react component if *.svg?url
+      use: ['@svgr/webpack'],
     });
 
     return config;
