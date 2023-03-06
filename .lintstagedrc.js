ec9ae6b865b12ce36c98e92041103794068d9c9a
CGQAQ <m.jason.liu@outlook.com>
2023-03-06 11:15:53 +0800

    fix: fix lintstagedrc

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,4 +1,4 @@
 module.exports = {
-  "**/*.{js,jsx,ts,tsx}": "npm run lint:fix",
-  "{src/pages,src/components,src/layout}/**/*.{js,jsx,ts,tsx}": "prettier --write",
+  "{src/pages,src/components,src/layout}/*.{js,jsx,ts,tsx}": "npm run lint:fix",
+  "{src/pages,src/components,src/layout}/**/*.{js,jsx,ts,tsx}": "npm run format:fix",
 };
