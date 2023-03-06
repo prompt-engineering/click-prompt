70e9cf4a266c55bd9c86c92e248770492a7b6023
CGQAQ <m.jason.liu@outlook.com>
2023-03-06 13:49:25 +0800

    chore: completely remove lintstaged

--- .lintstagedrc.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  "{src/pages,src/components,src/layout}/*.{js,jsx,ts,tsx}": "npm run lint:fix",
-  "{src/pages,src/components,src/layout}/**/*.{js,jsx,ts,tsx}": "npm run format:fix",
-};
