ea58130e17cb1233f23d1d473c28c5a793101d38
teobler <teobler@163.com>
2023-03-07 20:54:37 +0800

    Revert "Revert "feature: make ChatGPTApp rsc and get data from component level(#28)""
    This reverts commit 9475552dc663c0481be4cad9068d510ec1898272.

--- src/app/chatgpt/page.tsx
+++ src/app/chatgpt/page.tsx
@@ -1,12 +1,13 @@
-"use client";
-
 import React from "react";
 import { ChatGPTApp } from "@/components/ChatGPTApp";
 
-export default function ChatGPTPage() {
+function ChatGPTPage() {
   return (
     <div className='bg-[#343541] flex h-[85vh] overflow-y-auto'>
+      {/* @ts-expect-error Async Server Component */}
       <ChatGPTApp />
     </div>
   );
 }
+
+export default ChatGPTPage;
