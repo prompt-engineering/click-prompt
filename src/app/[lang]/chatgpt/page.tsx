b099869986e2aebe89ec5244a7b05980153228c2
genffy <genffyl@gmail.com>
2023-03-07 12:36:44 +0800

    fix: /chatgpt height

--- src/app/[lang]/chatgpt/page.tsx
+++ src/app/[lang]/chatgpt/page.tsx
@@ -5,7 +5,7 @@ import { ChatGPTApp } from "@/components/ChatGPTApp";
 
 export default function ChatGPTPage() {
   return (
-    <div className='bg-[#343541]'>
+    <div className='bg-[#343541] flex h-[85vh] overflow-y-auto'>
       <ChatGPTApp />
     </div>
   );
--- src/components/ChatGPTApp.tsx
+++ src/components/ChatGPTApp.tsx
@@ -3,11 +3,12 @@ import { Button, Input, Icon, useMediaQuery } from "@chakra-ui/react";
 
 import { ChatCompletionRequestMessage } from "openai";
 import { ArrowLeftIcon, ArrowRightIcon } from "@chakra-ui/icons";
-
+import Image from "next/image";
 import Send from "@/assets/icons/send.svg";
 import NewChat from "@/assets/icons/new-chat.svg";
 import TrashcanIcon from "@/assets/icons/trashcan.svg";
 import LogoutIcon from "@/assets/icons/logout.svg";
+import content from "@/assets/images/content.png";
 import styled from "@emotion/styled";
 
 const ChatInput = styled("input")`
@@ -78,128 +79,6 @@ const SendIcon = () => {
 type ChatGptAppProp = {
   message?: string;
 };
-// copy from https://chat.openai.com/chat
-function ChatGPTDesc({ onClick }: any) {
-  return (
-    <div className='text-gray-800 w-full md:flex md:flex-col px-6 dark:text-gray-100'>
-      <h1 className='text-4xl font-semibold text-center mt-6 sm:mt-[20vh] ml-auto mr-auto mb-10 sm:mb-16 flex gap-2 items-center justify-center'>
-        ChatGPT
-      </h1>
-      <div className='md:flex items-start text-center gap-3.5'>
-        <div className='flex flex-col mb-8 md:mb-auto gap-3.5 flex-1'>
-          <h2 className='flex gap-3 items-center m-auto text-lg font-normal md:flex-col md:gap-2'>
-            <svg
-              stroke='currentColor'
-              fill='none'
-              stroke-width='1.5'
-              viewBox='0 0 24 24'
-              stroke-linecap='round'
-              stroke-linejoin='round'
-              className='h-6 w-6'
-              height='1em'
-              width='1em'
-              xmlns='http://www.w3.org/2000/svg'
-            >
-              <circle cx='12' cy='12' r='5'></circle>
-              <line x1='12' y1='1' x2='12' y2='3'></line>
-              <line x1='12' y1='21' x2='12' y2='23'></line>
-              <line x1='4.22' y1='4.22' x2='5.64' y2='5.64'></line>
-              <line x1='18.36' y1='18.36' x2='19.78' y2='19.78'></line>
-              <line x1='1' y1='12' x2='3' y2='12'></line>
-              <line x1='21' y1='12' x2='23' y2='12'></line>
-              <line x1='4.22' y1='19.78' x2='5.64' y2='18.36'></line>
-              <line x1='18.36' y1='5.64' x2='19.78' y2='4.22'></line>
-            </svg>
-            Examples
-          </h2>
-          <ul className='flex flex-col gap-3.5 w-full sm:max-w-md m-auto'>
-            <button
-              className='w-full bg-gray-50 dark:bg-white/5 p-3 rounded-md hover:bg-gray-200 dark:hover:bg-gray-900'
-              onClick={() => onClick("Explain quantum computing in simple terms")}
-            >
-              &quot;Explain quantum computing in simple terms&quot; →
-            </button>
-            <button
-              className='w-full bg-gray-50 dark:bg-white/5 p-3 rounded-md hover:bg-gray-200 dark:hover:bg-gray-900'
-              onClick={() => onClick("Got any creative ideas for a 10 year old’s birthday?")}
-            >
-              &quot;Got any creative ideas for a 10 year old’s birthday?&quot; →
-            </button>
-            <button
-              className='w-full bg-gray-50 dark:bg-white/5 p-3 rounded-md hover:bg-gray-200 dark:hover:bg-gray-900'
-              onClick={() => onClick("How do I make an HTTP request in Javascript?")}
-            >
-              &quot;How do I make an HTTP request in Javascript?&quot; →
-            </button>
-          </ul>
-        </div>
-        <div className='flex flex-col mb-8 md:mb-auto gap-3.5 flex-1'>
-          <h2 className='flex gap-3 items-center m-auto text-lg font-normal md:flex-col md:gap-2'>
-            <svg
-              xmlns='http://www.w3.org/2000/svg'
-              fill='none'
-              viewBox='0 0 24 24'
-              stroke-width='1.5'
-              stroke='currentColor'
-              aria-hidden='true'
-              className='h-6 w-6'
-            >
-              <path
-                stroke-linecap='round'
-                stroke-linejoin='round'
-                d='M3.75 13.5l10.5-11.25L12 10.5h8.25L9.75 21.75 12 13.5H3.75z'
-              ></path>
-            </svg>
-            Capabilities
-          </h2>
-          <ul className='flex flex-col gap-3.5 w-full sm:max-w-md m-auto'>
-            <li className='w-full bg-gray-50 dark:bg-white/5 p-3 rounded-md'>
-              Remembers what user said earlier in the conversation
-            </li>
-            <li className='w-full bg-gray-50 dark:bg-white/5 p-3 rounded-md'>
-              Allows user to provide follow-up corrections
-            </li>
-            <li className='w-full bg-gray-50 dark:bg-white/5 p-3 rounded-md'>
-              Trained to decline inappropriate requests
-            </li>
-          </ul>
-        </div>
-        <div className='flex flex-col mb-8 md:mb-auto gap-3.5 flex-1'>
-          <h2 className='flex gap-3 items-center m-auto text-lg font-normal md:flex-col md:gap-2'>
-            <svg
-              stroke='currentColor'
-              fill='none'
-              stroke-width='1.5'
-              viewBox='0 0 24 24'
-              stroke-linecap='round'
-              stroke-linejoin='round'
-              className='h-6 w-6'
-              height='1em'
-              width='1em'
-              xmlns='http://www.w3.org/2000/svg'
-            >
-              <path d='M10.29 3.86L1.82 18a2 2 0 0 0 1.71 3h16.94a2 2 0 0 0 1.71-3L13.71 3.86a2 2 0 0 0-3.42 0z'></path>
-              <line x1='12' y1='9' x2='12' y2='13'></line>
-              <line x1='12' y1='17' x2='12.01' y2='17'></line>
-            </svg>
-            Limitations
-          </h2>
-          <ul className='flex flex-col gap-3.5 w-full sm:max-w-md m-auto'>
-            <li className='w-full bg-gray-50 dark:bg-white/5 p-3 rounded-md'>
-              May occasionally generate incorrect information
-            </li>
-            <li className='w-full bg-gray-50 dark:bg-white/5 p-3 rounded-md'>
-              May occasionally produce harmful instructions or biased content
-            </li>
-            <li className='w-full bg-gray-50 dark:bg-white/5 p-3 rounded-md'>
-              Limited knowledge of world and events after 2021
-            </li>
-          </ul>
-        </div>
-      </div>
-    </div>
-  );
-}
 
 export function ChatGPTApp(props: ChatGptAppProp) {
   const [isLoggedin, setIsLoggedin] = useState<boolean | null>(null);
@@ -381,7 +260,7 @@ export function ChatGPTApp(props: ChatGptAppProp) {
 
       {/* right */}
       <div className='relative flex flex-col items-center justify-start gap-16'>
-        {chatHistory.length === 0 && <ChatGPTDesc onClick={(text: string) => setMessage(text)} />}
+        {chatHistory.length === 0 && <Image className='mt-8' src={content} alt='background image'></Image>}
 
         {/* chats */}
         <ChatsWrapper ref={chatsWrapper} className='flex flex-col gap-4 w-full px-4 max-h-[70vh] overflow-y-auto mt-11'>
