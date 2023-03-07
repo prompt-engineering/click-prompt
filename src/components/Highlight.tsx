3e941b11c2e26e11f4fc5769303634cbb5fb5d20
abel <admin@sxyseo.com>
2023-03-07 19:20:16 +0800

    fixed: 修改成early return

--- src/components/Highlight.tsx
+++ src/components/Highlight.tsx
@@ -4,28 +4,24 @@ import React from "react";
  * Hight light keywords in paragraph
  */
 export default function Highlight({ value, keyword }: { value: string; keyword: string }) {
-  if (!keyword) {
+  if (!(value != undefined && keyword != undefined && value.length > 0 &&
+    keyword.length > 0)) {
     return value;
   }
   const regex = new RegExp(keyword, "gi");
-  if (value != undefined && keyword != undefined && value.length > 0 && 
-    keyword.length > 0) {
-    return value;
-  }
- 
+
   return value
     .split(regex)
     .reduce((acc: any, part: string, i: number) => {
-        if (i === 0) {
-          return [part];
-        }
-        return acc.concat(
-          <mark key={i} className='bg-yellow-300'>
-            {keyword}
-         </mark>,
+      if (i === 0) {
+        return [part];
+      }
+      return acc.concat(
+        <mark key={i} className='bg-yellow-300'>
+          {keyword}
+        </mark>,
         part,
       );
     }, [])
     .map((part: any, i: number) => <React.Fragment key={i}>{part}</React.Fragment>);
-  return valueStr;
 }
