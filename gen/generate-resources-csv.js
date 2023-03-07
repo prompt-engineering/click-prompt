815877db68fcee17d50ee88987c307758690e0ed
Phodal Huang <h@phodal.com>
2023-03-07 09:40:34 +0800

    fix: fix gen dir error issues

--- gen/generate-resources-csv.js
+++ gen/generate-resources-csv.js
@@ -4,18 +4,22 @@ const Papa = require("papaparse");
 // 1. loading the csv file from src/assets/data/ai-resources.csv
 // 2. parsing the csv file
 // 3. generating the json file to src/assets/data/ai-resources.json
-function convertCsvToJson(csvName) {
-  const csvFilePath = `./public/data/${csvName}.csv`;
-  const jsonFilePath = `./src/assets/resources/${csvName}.json`;
-  const csvFile = fs.readFileSync(csvFilePath, "utf8");
-  const json = Papa.parse(csvFile, {
-    header: true,
-    dynamicTyping: true,
-    skipEmptyLines: true,
+function convertCsvToJson(csvName, languages) {
+  languages.forEach((lang) => {
+    const csvFilePath = `./public/${lang}/data/${csvName}.csv`;
+    const jsonFilePath = `./src/assets/resources/${lang}/${csvName}.json`;
+    const csvFile = fs.readFileSync(csvFilePath, "utf8");
+    const json = Papa.parse(csvFile, {
+      header: true,
+      dynamicTyping: true,
+      skipEmptyLines: true,
+    });
+    fs.writeFileSync(jsonFilePath, JSON.stringify(json.data));
   });
-  fs.writeFileSync(jsonFilePath, JSON.stringify(json.data));
 }
 
-convertCsvToJson("ai-resources");
-convertCsvToJson("reading-list-en");
-convertCsvToJson("reading-list-cn");
+const languages = ["zh-cn", "en-us"];
+
+convertCsvToJson("ai-resources", languages);
+convertCsvToJson("reading-list-en", languages);
+convertCsvToJson("reading-list-cn", languages);
