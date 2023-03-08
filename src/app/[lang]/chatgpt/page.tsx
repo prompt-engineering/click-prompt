645d8326ee18df98c6bd2194b38c796b16ed9295
李瑞丰 <liruifeng1024@gmail.com>
2023-03-08 11:46:35 +0800

    feat: use key step (#72)
    * feat: use key step
    
    * fix: fix use className
    
    * docs: remove todos
    
    * feat: use key step
    
    * fix: fix use className
    
    * fix: fix use className
    
    ---------
    
    Co-authored-by: Phodal Huang <h@phodal.com>

--- src/app/[lang]/chatgpt/page.tsx
+++ src/app/[lang]/chatgpt/page.tsx
@@ -5,7 +5,7 @@ import { ChatGPTApp } from "@/components/ChatGPTApp";
 
 export default function ChatGPTPage() {
   return (
-    <div className='bg-[#343541] flex h-[85vh] overflow-y-auto'>
+    <div className='bg-[#343541] flex h-[85vh] overflow-y-auto rounded-md items-center justify-center'>
       <ChatGPTApp />
     </div>
   );
