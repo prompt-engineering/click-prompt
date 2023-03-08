e57f97ede398a5ef8cdbda4961086c9f4c6f999f
teobler <teobler@163.com>
2023-03-08 23:50:17 +0800

    fix: replace SITE_URL with localhost (https://github.com/vercel/next.js/issues/46840) (#28)
    Signed-off-by: teobler <teobler@163.com>

--- src/app/[lang]/chatgpt/page.tsx
+++ src/app/[lang]/chatgpt/page.tsx
@@ -1,24 +1,14 @@
 import React from "react";
 import { ChatRoom } from "@/app/[lang]/chatgpt/ChatRoom";
 import { LoginPage } from "@/app/[lang]/chatgpt/LoginPage";
-import { SITE_URL } from "@/configs/constants";
 
 export default async function ChatGPTPage() {
-  console.log("ChatGPTPage rendered");
+  const response = await fetch(`http://localhost:3000/api/chatgpt/verify`);
+  const data = await response.json();
 
-  try {
-    const response = await fetch(`${SITE_URL}api/chatgpt/verify`);
-    const data = await response.json();
-    console.log(data);
-
-    return (
-      <div className='bg-[#343541] flex h-[85vh] overflow-y-auto rounded-md items-center justify-center'>
-        {data.isLoggedIn ? <ChatRoom /> : <LoginPage />}
-      </div>
-    );
-  } catch (error) {
-    console.log(error);
-  }
-
-  return <></>;
+  return (
+    <div className='bg-[#343541] flex h-[85vh] overflow-y-auto rounded-md items-center justify-center'>
+      {data.isLoggedIn ? <ChatRoom /> : <LoginPage />}
+    </div>
+  );
 }
