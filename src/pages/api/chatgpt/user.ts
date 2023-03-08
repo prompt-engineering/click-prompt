32ecbe0f8503ab446cbaf6fa125941b874e8a45e
Phodal Huang <h@phodal.com>
2023-03-08 08:31:48 +0800

    refactor: simplify user with webstrom snashot

--- src/pages/api/chatgpt/user.ts
+++ src/pages/api/chatgpt/user.ts
@@ -19,10 +19,7 @@ let users: User[] = [];
 
 export function getUserByUserId(userId: string) {
   const user = users.find((user) => user.id === userId);
-  if (!user) {
-    return null;
-  }
-  return user;
+  return user ? user : null;
 }
 
 type Request = {
@@ -37,40 +34,46 @@ type Response = {
 
 const COOKIE_FOR_USER_ID = "PROMPT_GENERATOR_USER";
 const handler: NextApiHandler = async (req, res) => {
-  if (req.method === "POST" && req.body) {
-    const userId = req.cookies[COOKIE_FOR_USER_ID];
-    const { key, action } = typeof req.body === "string" ? JSON.parse(req.body) : req.body;
-    if (action) {
-      if (action === "login") {
-        if (!key) {
-          res.status(400).json({ error: "No key provided" } as Response);
-          return;
-        }
-        const userId = UUID();
-        users.push({
-          id: userId,
-          openai: createNewOpenAIApi(key),
-          conversations: new Map(),
-        });
-        console.log(`User ${userId} logged in`);
-        res.setHeader("Set-Cookie", `${COOKIE_FOR_USER_ID}=${userId}; Max-Age=3600;`);
-        return res.status(200).json({ message: "Logged in" } as Response);
-      } else if (action === "logout") {
-        if (!userId) {
-          res.status(200).json({ error: "You're not logged in yet!" } as Response);
-          return;
-        }
-
-        users = users.filter((user) => user.id !== userId);
-        return res.status(200).json({ message: "Logged out" } as Response);
-      } else {
-        res.status(400).json({ error: "Unknown action" } as Response);
-      }
-    } else {
-      res.status(400).json({ error: "No query provided" });
-    }
-  } else {
+  if (!(req.method === "POST" && req.body)) {
     res.status(404).json({ error: "Not found" });
+    return;
+  }
+
+  let userId = req.cookies[COOKIE_FOR_USER_ID];
+  const { key, action } = typeof req.body === "string" ? JSON.parse(req.body) : req.body;
+
+  if (!action) {
+    res.status(400).json({ error: "No query provided" });
+    return;
+  }
+
+  switch (action) {
+    case "login":
+      if (!key) {
+        res.status(400).json({ error: "No key provided" } as Response);
+        return;
+      }
+      userId = UUID();
+      users.push({
+        id: userId,
+        openai: createNewOpenAIApi(key),
+        conversations: new Map(),
+      });
+
+      console.log(`User ${userId} logged in`);
+      res.setHeader("Set-Cookie", `${COOKIE_FOR_USER_ID}=${userId}; Max-Age=3600;`);
+      return res.status(200).json({ message: "Logged in" } as Response);
+    case "logout":
+      if (!userId) {
+        res.status(200).json({ error: "You're not logged in yet!" } as Response);
+        return;
+      }
+
+      users = users.filter((user) => user.id !== userId);
+      return res.status(200).json({ message: "Logged out" } as Response);
+    default:
+      res.status(400).json({ error: "Unknown action" } as Response);
+      break;
   }
 };
 export default handler;
