8bad959304c6a5d1bc3a024f9a9b53c45985484b
Phodal Huang <h@phodal.com>
2023-03-11 15:02:15 +0800

    refactor: try to fix highlight issues

--- src/components/SimpleMarkdown.tsx
+++ src/components/SimpleMarkdown.tsx
@@ -19,11 +19,14 @@ function SimpleMarkdown({ content }: any) {
       components={{
         code({ node, inline, className, children, ...props }) {
           const match = /language-(\w+)/.exec(className || "");
+          // we had replace \n to \n\n for markdown to works, but it will cause a bug in syntax highlighter, so we need to return it back.
+          const code = String(children)?.replaceAll("\n\n", "\n");
+
           return !inline && match ? (
-            getHighlighter(match, props, children)
+            getHighlighter(match, props, code)
           ) : (
             <code className={className + " " + "empty-language"} {...props}>
-              {children}
+              {code}
             </code>
           );
         },
