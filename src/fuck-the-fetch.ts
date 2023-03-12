4f6f4d8d73107cb71bc7cbd5130494e6fcf7a57a
Jason <m.jason.liu@outlook.com>
2023-03-12 13:23:32 +0800

    fuck the fetch

--- src/fuck-the-fetch.ts
+++ src/fuck-the-fetch.ts
@@ -1,25 +1,8 @@
-import fetch, {
-  Blob,
-  blobFrom,
-  blobFromSync,
-  File,
-  fileFrom,
-  fileFromSync,
-  FormData,
-  Headers,
-  Request,
-  Response,
-} from "node-fetch";
+import fetch, { Blob, Headers, Request, Response } from "node-fetch";
 
 (fetch as any).__XX_PATCHED__ = true;
 (globalThis as any).fetch = fetch;
 (globalThis as any).Blob = Blob;
-(globalThis as any).blobFrom = blobFrom;
-(globalThis as any).blobFromSync = blobFromSync;
-(globalThis as any).File = File;
-(globalThis as any).fileFrom = fileFrom;
-(globalThis as any).fileFromSync = fileFromSync;
-(globalThis as any).FormData = FormData;
 (globalThis as any).Headers = Headers;
 (globalThis as any).Request = Request;
 (globalThis as any).Response = Response;
