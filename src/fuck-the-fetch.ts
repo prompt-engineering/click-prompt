65e234e49b6c092abb99c37de70d8acac4a62d09
Jason <m.jason.liu@outlook.com>
2023-03-12 13:26:24 +0800

    fuck the fetch

--- src/fuck-the-fetch.ts
+++ src/fuck-the-fetch.ts
@@ -2,7 +2,3 @@ import fetch, { Blob, Headers, Request, Response } from "node-fetch";
 
 (fetch as any).__XX_PATCHED__ = true;
 (globalThis as any).fetch = fetch;
-(globalThis as any).Blob = Blob;
-(globalThis as any).Headers = Headers;
-(globalThis as any).Request = Request;
-(globalThis as any).Response = Response;
