0a17647dcfa66cb94be9922136e9be7de11833bc
Phodal Huang <h@phodal.com>
2023-03-14 09:17:37 +0800

    feat: try to add pan & zoom

--- src/components/markdown/Mermaid.tsx
+++ src/components/markdown/Mermaid.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import svgPanZoom from "svg-pan-zoom";
 
 let currentId = 0;
 const uuid = () => `mermaid-${(currentId++).toString()}`;
@@ -6,18 +7,29 @@ const uuid = () => `mermaid-${(currentId++).toString()}`;
 export function Mermaid({ graphDefinition }: { graphDefinition: string }) {
   const [html, setHtml] = React.useState("");
   const [hasError, setHasError] = React.useState(false);
+  let currentId = uuid();
+
   React.useLayoutEffect(() => {
-    if (graphDefinition)
-      try {
-        (window as any).mermaid.mermaidAPI.render(uuid(), graphDefinition, (svgCode: any) => setHtml(svgCode));
-      } catch (e) {
-        setHtml("");
-        console.error(e);
-        setHasError(true);
-      }
+    if (!graphDefinition) {
+      return;
+    }
+
+    try {
+      (window as any).mermaid.mermaidAPI.render(currentId, graphDefinition, (svgCode: any) => setHtml(svgCode));
+    } catch (e) {
+      setHtml("");
+      // console.info(e);
+      setHasError(true);
+    }
   }, [graphDefinition]);
 
   if (hasError) return <code className={"mermaid"}>{graphDefinition}</code>;
+
+  setTimeout(() => {
+    document.getElementById(currentId)?.addEventListener("load", function () {
+      svgPanZoom(document.getElementById(currentId)!!);
+    });
+  }, 100);
 
   return graphDefinition ? <div dangerouslySetInnerHTML={{ __html: html }} /> : null;
 }
