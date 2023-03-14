51d4345962e0bf5454d2b22ac556bb7b66cc22c4
CGQAQ <m.jason.liu@outlook.com>
2023-03-14 11:05:26 +0800

    fix: fix panzoom

--- src/components/markdown/Mermaid.tsx
+++ src/components/markdown/Mermaid.tsx
@@ -1,38 +1,37 @@
-import React from "react";
+import "client-only";
+import React, { useEffect, useRef } from "react";
 import svgPanZoom from "svg-pan-zoom";
 
 let currentId = 0;
 const uuid = () => `mermaid-${(currentId++).toString()}`;
 
 export function Mermaid({ graphDefinition }: { graphDefinition: string }) {
-  const [html, setHtml] = React.useState("");
+  const ref = useRef<HTMLDivElement>(null);
   const [hasError, setHasError] = React.useState(false);
   const currentId = uuid();
 
-  React.useLayoutEffect(() => {
+  useEffect(() => {
     if (!graphDefinition) {
       return;
     }
 
     try {
-      (window as any).mermaid.mermaidAPI.render(currentId, graphDefinition, (svgCode: any) => setHtml(svgCode));
+      (window as any).mermaid.mermaidAPI.render(currentId, graphDefinition, (svgCode: string) => {
+        ref.current!.innerHTML = svgCode;
+      });
+      const it = document.getElementById(currentId);
+      // console.log("it", typeof it);
+      // eslint-disable-next-line  @typescript-eslint/no-extra-non-null-assertion
+      const instance = svgPanZoom(it!!);
+
+      return () => instance.destroy();
     } catch (e) {
-      setHtml("");
-      // console.info(e);
+      console.info(e);
       setHasError(true);
     }
   }, [graphDefinition]);
 
   if (hasError) return <code className={"mermaid"}>{graphDefinition}</code>;
 
-  setTimeout(() => {
-    document.getElementById(currentId)?.addEventListener("load", function () {
-      if (document.getElementById(currentId)) {
-        // eslint-disable-next-line  @typescript-eslint/no-extra-non-null-assertion
-        svgPanZoom(document.getElementById(currentId)!!);
-      }
-    });
-  }, 100);
-
-  return graphDefinition ? <div dangerouslySetInnerHTML={{ __html: html }} /> : null;
+  return <div ref={ref}> </div>;
 }
