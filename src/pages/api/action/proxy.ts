3cae6cdf14b9c27424a947c223ee4fddb367c2c5
Phodal Huang <h@phodal.com>
2023-03-16 22:28:15 +0800

    feat: align GitHub api for working flow #117

--- src/pages/api/action/proxy.ts
+++ src/pages/api/action/proxy.ts
@@ -1,21 +1,38 @@
 import { NextApiHandler } from "next";
 import fetch from "node-fetch";
 
+export type ApiAction = {
+  url: string;
+  method: string;
+  headers: {
+    name: string;
+    value: string;
+  }[];
+  body: string;
+};
+
 const handler: NextApiHandler = async (req, res) => {
   if (req.method !== "POST" || !req.body) {
     res.status(400).json({ error: "Invalid request" });
     return;
   }
-  const { url, method, headers, body } = req.body;
+  const { url, method, headers, body } = req.body as ApiAction;
 
   const proxy_body: any = typeof body === "string" ? JSON.parse(body) : body;
-  const response = await fetch(url, {
-    method,
-    // todo: convert headers
-    body: JSON.stringify(proxy_body),
-  });
+  const [response] = await Promise.all([
+    fetch(url, {
+      method,
+      // todo: convert headers
+      headers: headers.reduce((acc, { name, value }) => {
+        (acc as any)[name] = value;
+        return acc;
+      }),
+      body: JSON.stringify(proxy_body),
+    }),
+  ]);
 
-  console.log("create proxy request", url, method);
+  console.log("create proxy request: ", method, url, headers, body);
+  console.log("proxy response: ", response.status, response.statusText, await response.text());
 
   if (response.ok) {
     const { headers, body } = await response.json();
