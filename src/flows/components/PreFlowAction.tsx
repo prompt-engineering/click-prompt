08e8e99b6ab2a317f1d9fec841281521b95b0415
Phodal Huang <h@phodal.com>
2023-03-17 23:06:54 +0800

    feat: make auth input form works #117

--- src/flows/components/PreFlowAction.tsx
+++ src/flows/components/PreFlowAction.tsx
@@ -1,31 +1,60 @@
-import React from "react";
+import React, { Fragment } from "react";
 import { AuthKeyValues, FlowAction } from "@/flows/types/flow-action";
-import { Button } from "@chakra-ui/react";
+import { Button, FormControl, Grid, Input } from "@chakra-ui/react";
 import { preActionDispatcher } from "../pre-action-dispatcher";
 import { parseConfigures } from "@/flows/components/SettingHeaderConfig";
+import { Form, Formik, useFormik } from "formik";
 
 type ActionProps = { action: FlowAction; onResponse?: (value: any) => void };
 
 function PreFlowAction({ action }: ActionProps) {
-  let configures: AuthKeyValues = [];
+  let conf: AuthKeyValues = [];
   if (action.api?.headers) {
-    configures = parseConfigures(action.api?.headers);
+    conf = parseConfigures(action.api?.headers);
   }
 
-  const clickButton = () => {
-    preActionDispatcher(action).then((r) => console.log(r));
-  };
+  const formik = useFormik({
+    initialValues: conf.reduce((acc: any, field) => {
+      acc[field.key] = field.value;
+      return acc;
+    }, {}),
+    onSubmit: (values) => {
+      if (conf.length > 0 && action.api?.headers) {
+        action.api!.headers.map((header) => {
+          if (values[header.key]) {
+            header.value = values[header.key];
+          }
+        });
+      }
+
+      console.log(action);
+      preActionDispatcher(action).then((r) => console.log(r));
+    },
+  });
 
   return (
-    <div>
-      {configures.length > 0 &&
-        configures.map((c) => {
-          return <></>;
-        })}
-      <Button colorScheme='twitter' className='bg-blue' onClick={clickButton}>
-        {action.name}
-      </Button>
-    </div>
+    <form onSubmit={formik.handleSubmit}>
+      <Grid gap={6} alignItems='end'>
+        {conf.length > 0 &&
+          conf.map((keyValue, index) => (
+            <Fragment key={index}>
+              <FormControl key={keyValue.key} id={keyValue.key} mt={4}>
+                <label htmlFor={keyValue.key}>{keyValue.key}</label>
+                <Input
+                  onChange={(event) => {
+                    const inputValue = event.target.value;
+                    formik.setFieldValue(keyValue.key, inputValue);
+                  }}
+                />
+              </FormControl>
+            </Fragment>
+          ))}
+
+        <Button colorScheme='twitter' className='bg-blue' type='submit'>
+          {action.name}
+        </Button>
+      </Grid>
+    </form>
   );
 }
 
