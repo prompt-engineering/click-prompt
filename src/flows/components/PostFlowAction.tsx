eb4aa367046b28272db620eafe0ee9c52fb1cce3
Phodal Huang <h@phodal.com>
2023-03-17 23:13:19 +0800

    refactor: extract shared flow action #117

--- src/flows/components/PostFlowAction.tsx
+++ src/flows/components/PostFlowAction.tsx
@@ -2,23 +2,16 @@ import React from "react";
 import { FlowAction } from "@/flows/types/flow-action";
 import { Button } from "@chakra-ui/react";
 import { postActionDispatcher } from "@/flows/post-action-dispatcher";
+import SharedFlowAction from "@/flows/components/SharedFlowAction";
 
 type ActionProps = { action: FlowAction; response: string };
 
 function PostFlowAction({ action, response }: ActionProps) {
-  const gptResponse = response;
-
-  const clickButton = () => {
-    postActionDispatcher(action, gptResponse).then((r) => console.log(r));
+  const handleSubmit = (modifiedAction: FlowAction) => {
+    postActionDispatcher(modifiedAction, response).then((r) => console.log(r));
   };
 
-  return (
-    <div>
-      <Button colorScheme='twitter' className='bg-blue' onClick={clickButton}>
-        {action.name}
-      </Button>
-    </div>
-  );
+  return <SharedFlowAction action={action} onSubmit={handleSubmit} />;
 }
 
 export default PostFlowAction;
--- src/flows/components/PreFlowAction.tsx
+++ src/flows/components/PreFlowAction.tsx
@@ -1,61 +1,16 @@
-import React, { Fragment } from "react";
-import { AuthKeyValues, FlowAction } from "@/flows/types/flow-action";
-import { Button, FormControl, Grid, Input } from "@chakra-ui/react";
+import React from "react";
+import { FlowAction } from "@/flows/types/flow-action";
 import { preActionDispatcher } from "../pre-action-dispatcher";
-import { parseConfigures } from "@/flows/components/SettingHeaderConfig";
-import { Form, Formik, useFormik } from "formik";
+import SharedFlowAction from "./SharedFlowAction";
 
 type ActionProps = { action: FlowAction; onResponse?: (value: any) => void };
 
 function PreFlowAction({ action }: ActionProps) {
-  let conf: AuthKeyValues = [];
-  if (action.api?.headers) {
-    conf = parseConfigures(action.api?.headers);
-  }
+  const handleSubmit = (modifiedAction: FlowAction) => {
+    preActionDispatcher(modifiedAction).then((r) => console.log(r));
+  };
 
-  const formik = useFormik({
-    initialValues: conf.reduce((acc: any, field) => {
-      acc[field.key] = field.value;
-      return acc;
-    }, {}),
-    onSubmit: (values) => {
-      if (conf.length > 0 && action.api?.headers) {
-        action.api!.headers.map((header) => {
-          if (values[header.key]) {
-            header.value = values[header.key];
-          }
-        });
-      }
-
-      console.log(action);
-      preActionDispatcher(action).then((r) => console.log(r));
-    },
-  });
-
-  return (
-    <form onSubmit={formik.handleSubmit}>
-      <Grid gap={6} alignItems='end'>
-        {conf.length > 0 &&
-          conf.map((keyValue, index) => (
-            <Fragment key={index}>
-              <FormControl key={keyValue.key} id={keyValue.key} mt={4}>
-                <label htmlFor={keyValue.key}>{keyValue.key}</label>
-                <Input
-                  onChange={(event) => {
-                    const inputValue = event.target.value;
-                    formik.setFieldValue(keyValue.key, inputValue);
-                  }}
-                />
-              </FormControl>
-            </Fragment>
-          ))}
-
-        <Button colorScheme='twitter' className='bg-blue' type='submit'>
-          {action.name}
-        </Button>
-      </Grid>
-    </form>
-  );
+  return <SharedFlowAction action={action} onSubmit={handleSubmit} />;
 }
 
 export default PreFlowAction;
